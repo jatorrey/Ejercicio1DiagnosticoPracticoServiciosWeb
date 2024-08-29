686176c97ef60e53494f4f16a1966cb7d424f983
jatorrey <jacatorresre@ittepic.edu.mx>
2024-08-28 18:01:19 -0700

    Primer ejercicio javascript

---
+++ Ejercicio 1.js
@@ -0,0 +1,8 @@
+// 1. Conteo de caracteres
+// Torres Reyes Jaime Camil
+function contarCaracteres(cadena) {
+    return cadena.length;
+}
+
+const texto = "Hola, ¿cómo estás?";
+console.log("Número de caracteres:", contarCaracteres(texto));
